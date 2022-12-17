bca0f4c83f6d7db8a2499a1d51b21e00e8d66550
Sami <muhammadsami112@gmail.com>
2022-12-17 20:49:11 +0500

    Samikhan

---
+++ Basic_Laravel/database/migrations/2022_12_17_131953_create_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('create_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('ProductName',225);
+            $table->string('Email',225);
+            $table->decimal('Price');
+            $table->text('Description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('create_product');
+    }
+};
